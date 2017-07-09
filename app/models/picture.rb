eeb891c6bb94de4d12f35da2d082c563714e3faa
tomnatt <github@tomnatt.com>
2017-07-09 16:16:56 +0100

    Add default values for Picture

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -7,4 +7,12 @@ class Picture < ApplicationRecord
             :month,
             :year,
             :image, presence: true
+
+  after_initialize :default_values
+
+  def default_values
+    # Default month to last month
+    self.month ||= Date.current.month - 1
+    self.year ||= Date.current.year
+  end
 end
