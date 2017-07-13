c3f46114f7e53fa4b670b991d16cd655e484c39b
tomnatt <github@tomnatt.com>
2017-07-13 16:22:41 +0100

    Validate month input

--- app/controllers/month_controller.rb
+++ app/controllers/month_controller.rb
@@ -1,7 +1,8 @@
 class MonthController < ApplicationController
   def show
-    # TODO: Add some sanitization here
+    @pictures = []
     month = params[:month]
+    return unless month =~ /\A\d{1,2}\z/
     @pictures = Picture.where(month: month).order('photographer ASC')
   end
 end
