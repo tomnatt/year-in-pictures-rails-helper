98f9d6ed598dac3d9eef9a3d97310fb19619799f
tomnatt <github@tomnatt.com>
2017-12-12 17:31:38 +0000

    Add placeholders automatically in the month view
    First pass. Instead of adding them manually as database entries.

--- app/controllers/month_controller.rb
+++ app/controllers/month_controller.rb
@@ -3,6 +3,15 @@ class MonthController < ApplicationController
     @pictures = []
     month = params[:month]
     return unless month =~ /\A\d{1,2}\z/
-    @pictures = Picture.where(month: month).order('photographer ASC')
+
+    # Get the pics and create placeholder entries for any missing
+    @pictures = Picture.where(month: month).to_a
+    Picture.photographers.each do |photographer|
+      next if @pictures.any? { |pic| pic.photographer == photographer }
+      @pictures << Picture.placeholder(photographer)
+    end
+
+    # Sort by photographer
+    @pictures.sort_by(&:photographer)
   end
 end
