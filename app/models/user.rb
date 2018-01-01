45aa53a173bf429e2be95f21250f8daf3b6edc22
tomnatt <github@tomnatt.com>
2018-01-01 18:25:57 +0000

    Disabled user can no longer authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,16 @@ class User < ApplicationRecord
   enum role: %i[disabled user admin]
   after_initialize :set_default_role, if: :new_record?
 
+  # Called by Devise to ensure current user is ok to authenticate
+  def active_for_authentication?
+    super && !disabled?
+  end
+
+  # Custom message for when account disabled
+  def inactive_message
+    'Account is disabled'
+  end
+
   def self.find_by_fullname_or_placeholder(fullname)
     user = User.find_by(fullname: fullname)
     return user unless user.nil?
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,7 +10,6 @@ class UserTest < ActiveSupport::TestCase
 
   test 'default to placeholder user if no user found' do
     user = User.find_by_fullname_or_placeholder('random')
-
     assert_equal 'random', user.fullname
     assert_equal 'nothing@example.com', user.email
     assert_nil user.id, 'User id should be nil as it is not in database'
@@ -24,5 +23,20 @@ class UserTest < ActiveSupport::TestCase
   test 'fullname must be unique case insensitive' do
     user = users(:duplicate_fullname_insensitive_user)
     refute user.valid?
+  end
+
+  test 'admin user is active for authentication' do
+    user = users(:admin_user)
+    assert user.active_for_authentication?
+  end
+
+  test 'normal user is active for authentication' do
+    user = users(:normal_user)
+    assert user.active_for_authentication?
+  end
+
+  test 'disabled user is not active for authentication' do
+    user = users(:disabled_user)
+    refute user.active_for_authentication?
   end
 end
