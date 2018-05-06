9119d3327e563ce448c09eb82d4527eb17863e49
tomnatt <github@tomnatt.com>
2018-05-06 18:28:49 +0100

    Add tests around deleting pictures

--- test/controllers/pictures_controller_test.rb
+++ test/controllers/pictures_controller_test.rb
@@ -65,9 +65,27 @@ class PicturesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to picture_url(@picture)
   end
 
-  test 'should destroy picture' do
+  test 'should destroy picture if owner' do
     assert_difference('Picture.count', -1) do
-      delete picture_url(@picture)
+      delete picture_url(pictures(:user_one_picture_to_delete))
+    end
+
+    assert_redirected_to pictures_url
+  end
+
+  test 'should not destroy picture if not owner' do
+    assert_no_difference('Picture.count') do
+      delete picture_url(pictures(:user_two_picture_cant_delete))
+    end
+
+    assert_redirected_to root_url
+  end
+
+  test 'admin should be able to delete any picture' do
+    sign_out @user
+    login_as users(:admin_user)
+    assert_difference('Picture.count', -1) do
+      delete picture_url(pictures(:user_one_picture_to_delete))
     end
 
     assert_redirected_to pictures_url
