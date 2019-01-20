794495a6f9b5a6802d4170c3b1a6359356d38c46
tomnatt <github@tomnatt.com>
2019-01-20 17:39:48 +0000

    Update month view to take year URL parameter
    Also remove placeholders from listing. Tests updated.

--- app/controllers/month_controller.rb
+++ app/controllers/month_controller.rb
@@ -4,21 +4,28 @@ class MonthController < ApplicationController
   def show
     @pictures = []
     month = params[:month]
+    year = get_year(params[:year])
     return unless month =~ /\A\d{1,2}\z/
-    @pictures = get_pictures(month)
+
+    @pictures = get_pictures(month, year)
   end
 
   private
 
-  # Get the pics and create placeholder entries for any missing
-  def get_pictures(month)
-    pictures = Picture.where(month: month).to_a
-    User.all_active.each do |photographer|
-      next if pictures.any? { |pic| pic.user == photographer }
-      pictures << Picture.placeholder(photographer.fullname)
-    end
+  # Get the pics for the given year and sort by photographer
+  def get_pictures(month, year)
+    pictures = Picture.where(month: month, year: year).to_a
+    pictures = pictures.reject { |pic| pic.user.disabled? }
 
     # Sort by photographer
     pictures.sort_by! { |pic| pic.user.fullname }
+  end
+
+  # Sanitize input year or return this year
+  def get_year(input_year)
+    current_year = Time.now.year
+    return input_year if input_year.to_i.in?((2018..current_year).to_a)
+
+    current_year
   end
 end
