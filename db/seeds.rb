6366a4c9daa54b1a3946784059423a050ec43a0e
tomnatt <github@tomnatt.com>
2022-04-29 16:31:53 +0100

    Update syntax for new rubocop definitions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-admin_email = ENV['YIP_HELPER_EMAIL'] || 'admin@example.com'
-admin_password = ENV['YIP_HELPER_PASSWORD'] || 'password'
-admin_name = ENV['YIP_HELPER_NAME'] || 'admin'
+admin_email = ENV.fetch('YIP_HELPER_EMAIL', 'admin@example.com')
+admin_password = ENV.fetch('YIP_HELPER_PASSWORD', 'password')
+admin_name = ENV.fetch('YIP_HELPER_NAME', 'admin')
 User.create(email:    admin_email,
             password: admin_password,
             fullname: admin_name,
