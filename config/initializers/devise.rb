7dbc90dfc3ddf8e28521872e7a7827f3410e91bc
tomnatt <github@tomnatt.com>
2022-11-27 12:44:51 +0000

    Updates for moving hosting to Koyeb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   config.mailer_sender = 'webmaster@theyearinpictures.co.uk'
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
