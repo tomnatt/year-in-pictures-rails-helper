260d21b1452fe513dbf7d5e34e6f577638bb4014
tomnatt <github@tomnatt.com>
2026-03-17 19:53:01 +0000

    Update ruby and gem versions
    And correct for linter.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -51,7 +51,7 @@ class PicturesController < ApplicationController
         format.json { render :show, status: :created, location: @picture }
       else
         format.html { render :new }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
+        format.json { render json: @picture.errors, status: :unprocessable_content }
       end
     end
   end
@@ -69,7 +69,7 @@ class PicturesController < ApplicationController
         format.json { render :show, status: :ok, location: @picture }
       else
         format.html { render :edit }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
+        format.json { render json: @picture.errors, status: :unprocessable_content }
       end
     end
   end
